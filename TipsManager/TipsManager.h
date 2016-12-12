fbf9f23995c6be5b6439f0056816a8ced8d90206
CodeEagle <stasura@hotmail.com>
2016-12-12 21:02:24 +0800

    back

---
+++ TipsManager/TipsManager.h
@@ -0,0 +1,19 @@
+//
+//  TipsManager.h
+//  TipsManager
+//
+//  Created by LawLincoln on 16/6/1.
+//  Copyright © 2016年 SelfStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TipsManager.
+FOUNDATION_EXPORT double TipsManagerVersionNumber;
+
+//! Project version string for TipsManager.
+FOUNDATION_EXPORT const unsigned char TipsManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TipsManager/PublicHeader.h>
+
+
